b33f3950c352a348fb32dd6f42e3c94c0aee55c6
gabGodiskova <gabgodiskova@gmail.com>
2021-12-02 10:47:37 +0000

    Deploying to docs from @ books-are-next/soucek-tuseni-souvislosti@e3cffafce19ee4096f0c96c942051ecece66b59e 🚀

---
+++ epub2nb-output/content/tuseni_souvislosti_032.md
@@ -0,0 +1,7 @@
+---
+title: ''
+---
+
+![032.jpg](./resources/032_fmt.jpeg)
+
+Pečeť z Karanova[1](./resources/undefined)
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-e3cffaf';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./tuseni_souvislosti_002.html","./tuseni_souvislosti_003.html","./tuseni_souvislosti_005.html","./tuseni_souvislosti_006.html","./tuseni_souvislosti_007.html","./tuseni_souvislosti_008.html","./tuseni_souvislosti_009.html","./tuseni_souvislosti_010.html","./tuseni_souvislosti_011.html","./tuseni_souvislosti_012.html","./tuseni_souvislosti_013.html","./tuseni_souvislosti_014.html","./tuseni_souvislosti_015.html","./tuseni_souvislosti_016.html","./tuseni_souvislosti_017.html","./tuseni_souvislosti_018.html","./tuseni_souvislosti_019.html","./tuseni_souvislosti_020.html","./tuseni_souvislosti_021.html","./tuseni_souvislosti_022.html","./tuseni_souvislosti_023.html","./tuseni_souvislosti_024.html","./tuseni_souvislosti_025.html","./tuseni_souvislosti_026.html","./tuseni_souvislosti_027.html","./tuseni_souvislosti_028.html","./tuseni_souvislosti_029.html","./tuseni_souvislosti_030.html","./tuseni_souvislosti_031.html","./tuseni_souvislosti_032.html","./tuseni_souvislosti_033.html","./tuseni_souvislosti_034.html","./tuseni_souvislosti_035.html","./tuseni_souvislosti_036.html","./tuseni_souvislosti_037.html","./tuseni_souvislosti_038.html","./tuseni_souvislosti_039.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/005_fmt.jpeg","./resources/006_fmt.jpeg","./resources/004_fmt.jpeg","./resources/007_fmt.jpeg","./resources/008_fmt.jpeg","./resources/009_fmt.jpeg","./resources/010_fmt.jpeg","./resources/011_fmt.jpeg","./resources/012_fmt.jpeg","./resources/013_fmt.jpeg","./resources/014_fmt.jpeg","./resources/015_fmt.jpeg","./resources/016_fmt.jpeg","./resources/017_fmt.jpeg","./resources/018_fmt.jpeg","./resources/019_fmt.jpeg","./resources/020_fmt.jpeg","./resources/021_fmt.jpeg","./resources/022_fmt.jpeg","./resources/023_fmt.jpeg","./resources/024_fmt.jpeg","./resources/025_fmt.jpeg","./resources/026_fmt.jpeg","./resources/027_fmt.jpeg","./resources/028_fmt.jpeg","./resources/029_fmt.jpeg","./resources/030_fmt.jpeg","./resources/031_fmt.jpeg","./resources/032_fmt.jpeg","./resources/033_fmt.jpeg","./resources/034_fmt.jpeg","./resources/035_fmt.jpeg","./resources/036_fmt.jpeg","./resources/037_fmt.jpeg","./resources/038_fmt.jpeg","./resources/039_fmt.jpeg","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/obalka_tuseni_souvislo_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
